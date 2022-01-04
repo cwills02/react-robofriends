b77de76bb65795793dba50c6eebb2f32f5c43f78
Corey Wills <cwills788@gmail.com>
2022-01-04 16:57:07 -0600

    Updates

---
+++ precache-manifest.cff71437e79950e1ae5d50a0d00c17e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e823d1ac61835e1e7f0",
+    "url": "/react-robofriends/static/css/main.ecbeb61a.chunk.css"
+  },
+  {
+    "revision": "2e823d1ac61835e1e7f0",
+    "url": "/react-robofriends/static/js/main.2e823d1a.chunk.js"
+  },
+  {
+    "revision": "9e3b18bddc752fdd93e4",
+    "url": "/react-robofriends/static/css/1.f6506120.chunk.css"
+  },
+  {
+    "revision": "9e3b18bddc752fdd93e4",
+    "url": "/react-robofriends/static/js/1.9e3b18bd.chunk.js"
+  },
+  {
+    "revision": "6755765800e2384af4d2",
+    "url": "/react-robofriends/static/js/runtime~main.67557658.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "7e99ed7a6e39ffd3e1a2102a59d9baf3",
+    "url": "/react-robofriends/index.html"
+  }
+];
